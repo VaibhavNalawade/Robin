ef25189e9b78d15430892b233f7247199e4c7606
vaibhav <vaibhavnalawade@outlook.in>
2022-12-22 10:51:49 +0530

    updated Drawer Icons

--- app/src/main/java/com/vaibhav/robin/presentation/ui/common/DrawerContent.kt
+++ app/src/main/java/com/vaibhav/robin/presentation/ui/common/DrawerContent.kt
@@ -1,6 +1,8 @@
 package com.vaibhav.robin.presentation.ui.common
 
+import android.app.Activity
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
@@ -11,6 +13,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
@@ -38,154 +41,160 @@ fun DrawerContent(
     signOut: () -> Unit,
     closeDrawer: () -> Unit
 ) {
-    val scope = rememberCoroutineScope()
     val route = navController.currentBackStackEntryAsState().value?.destination?.route
-    val scrollState = rememberScrollState()
-    Column(
-        modifier = Modifier
-            .statusBarsPadding()
-            .width(320.dp)
-    ) {
-        SpacerVerticalTwo()
-        Text(
-            modifier = Modifier.padding(horizontal = Dimens.gird_two),
-            text = stringResource(id = R.string.app_name),
-            style = typography.titleLarge,
-            color = colorScheme.primary
-        )
-        SpacerVerticalTwo()
+    val ctx = LocalContext.current
+
+    Surface(tonalElevation = Dimens.surface_elevation_1) {
         Column(
-            modifier = Modifier.verticalScroll(
-                state = scrollState
-            )
+            modifier = Modifier
+                .width(360.dp)
+                .fillMaxHeight()
+                .statusBarsPadding()
         ) {
-            NavigationDrawerItem(
-                shape = Shapes.NavigationItemShape,
-                modifier = Modifier.padding(end = Dimens.gird_two),
-                icon = {
-                    Icon(
-                        painterResource(id = R.drawable.home_fill0_wght700_grad0_opsz24),
-                        contentDescription = null
-                    )
-                },
-                label = { Text("Home") },
-                selected = route == RobinDestinations.HOME,
-                onClick = {
-                    if (route != RobinDestinations.HOME)
-                        navController.navigate(RobinDestinations.HOME) {
-                            popUpTo(RobinDestinations.HOME) {
-                                inclusive = true
+            SpacerVerticalTwo()
+            Text(
+                modifier = Modifier.padding(horizontal = Dimens.gird_two),
+                text = stringResource(id = R.string.app_name),
+                style = typography.titleLarge,
+                color = colorScheme.primary
+            )
+            SpacerVerticalTwo()
+            Column(
+                modifier = Modifier.verticalScroll(state = rememberScrollState())
+            ) {
+                NavigationDrawerItem(
+                    shape = Shapes.NavigationItemShape,
+                    modifier = Modifier.padding(end = Dimens.gird_two),
+                    selected = route == RobinDestinations.HOME,
+                    icon = {
+                        Icon(
+                            painterResource(id = R.drawable.home),
+                            contentDescription = null
+                        )
+                    },
+                    label = {
+                        Text(text = stringResource(id = R.string.home))
+                    },
+                    onClick = {
+                        if (route != RobinDestinations.HOME)
+                            navController.navigate(RobinDestinations.HOME) {
+                                popUpTo(RobinDestinations.HOME) {
+                                    inclusive = true
+                                }
                             }
-                        }
-                    closeDrawer()
-                }
-            )
-            NavigationDrawerItem(
-                shape = Shapes.NavigationItemShape,
-                modifier = Modifier.padding(end = Dimens.gird_two),
-                icon = {
-                    Icon(
-                        painterResource(id = R.drawable.shopping_bag_fill0_wght700_grad0_opsz24),
-                        contentDescription = null
-                    )
-                },
-                label = { Text("Your Orders") },
-                selected = false,
-                onClick = {
-
-                    closeDrawer()
-
-                }
-            )
-
-
-            NavigationDrawerItem(
-                shape = Shapes.NavigationItemShape,
-                modifier = Modifier.padding(end = Dimens.gird_two),
-                icon = {
-                    Icon(
-                        painterResource(
-                            id = R.drawable.shopping_cart_fill0_wght700_grad0_opsz24
-                        ),
-                        contentDescription = null
-                    )
-                },
-                label = { Text("Cart") },
-                selected = route == RobinDestinations.CART,
-                onClick = {
-                    if (route != RobinDestinations.CART)
-                        navController.navigate(RobinDestinations.CART)
-                    closeDrawer()
-                }
-            )
-
-            NavigationDrawerItem(
-                shape = Shapes.NavigationItemShape,
-                modifier = Modifier.padding(end = Dimens.gird_two),
-                icon = {
-                    Icon(
-                        painterResource(
-                            id = R.drawable.supervised_user_circle_fill0_wght700_grad0_opsz24
-                        ),
-                        contentDescription = null
-                    )
-                },
-                label = { Text("Profile") },
-                selected = false,
-                onClick = {
-                    closeDrawer()
-                },
-
-                )
-
-            NavigationDrawerItem(
-                shape = Shapes.NavigationItemShape,
-                modifier = Modifier.padding(end = Dimens.gird_two),
-                icon = {
-                    Icon(
-                        painterResource(
-                            id = R.drawable.settings_fill0_wght700_grad0_opsz24
-                        ),
-                        contentDescription = null
-                    )
-                },
-                label = { Text("Settings") },
-                selected = false,
-                onClick = {
-                    closeDrawer()
-                },
-
-                )
-
-            NavigationDrawerItem(
-                shape = Shapes.NavigationItemShape,
-                modifier = Modifier.padding(end = Dimens.gird_two),
-                icon = {
-                    Icon(
-                        painterResource(
-                            id = R.drawable.support_fill0_wght700_grad0_opsz24
-                        ),
-                        contentDescription = null
-                    )
-                },
-                label = { Text("Help") },
-                selected = false,
-                onClick = {
-                    closeDrawer()
-                },
-
-                )
-            AuthUserNavigationItem(
-                userAuthenticated,
-                signOut = signOut,
-                selected = route == RobinDestinations.LOGIN,
-                signIn = {
-                    if (route != RobinDestinations.LOGIN) {
-                        navController.navigate(RobinDestinations.LOGIN)
                         closeDrawer()
                     }
-                }
-            )
+                )
+                NavigationDrawerItem(
+                    shape = Shapes.NavigationItemShape,
+                    modifier = Modifier.padding(end = Dimens.gird_two),
+                    selected = false,
+                    onClick = closeDrawer,
+                    icon = {
+                        Icon(
+                            painterResource(id = R.drawable.shopping_bag),
+                            contentDescription = null
+                        )
+                    },
+                    label = {
+                        Text(stringResource(id = R.string.your_orders))
+                    }
+                )
+                NavigationDrawerItem(
+                    shape = Shapes.NavigationItemShape,
+                    modifier = Modifier.padding(end = Dimens.gird_two),
+                    selected = route == RobinDestinations.CART,
+                    icon = {
+                        Icon(
+                            painterResource(
+                                id = R.drawable.shopping_cart
+                            ),
+                            contentDescription = null
+                        )
+                    },
+                    label = {
+                        Text(stringResource(id = R.string.cart))
+                    },
+                    onClick = {
+                        if (route != RobinDestinations.CART)
+                            navController.navigate(RobinDestinations.CART)
+                        closeDrawer()
+                    }
+                )
+                NavigationDrawerItem(
+                    shape = Shapes.NavigationItemShape,
+                    modifier = Modifier.padding(end = Dimens.gird_two),
+                    selected = false,
+                    onClick = closeDrawer,
+                    icon = {
+                        Icon(
+                            painterResource(
+                                id = R.drawable.account_circle
+                            ),
+                            contentDescription = null
+                        )
+                    },
+                    label = {
+                        Text(stringResource(id = R.string.profile))
+                    }
+                )
+                AuthUserNavigationItem(
+                    userAuthenticated,
+                    signOut = signOut,
+                    selected = route == RobinDestinations.LOGIN,
+                    signIn = {
+                        if (route != RobinDestinations.LOGIN) {
+                            navController.navigate(RobinDestinations.LOGIN)
+                            closeDrawer()
+                        }
+                    }
+                )
+                DividerHorizontal()
+                NavigationDrawerItem(
+                    shape = Shapes.NavigationItemShape,
+                    modifier = Modifier.padding(end = Dimens.gird_two),
+                    icon = {
+                        Icon(
+                            painterResource(
+                                id = R.drawable.settings
+                            ),
+                            contentDescription = null
+                        )
+                    },
+                    label = { Text("Settings") },
+                    selected = false,
+                    onClick = closeDrawer,
+                )
+                NavigationDrawerItem(
+                    shape = Shapes.NavigationItemShape,
+                    modifier = Modifier.padding(end = Dimens.gird_two),
+                    icon = {
+                        Icon(
+                            painterResource(id = R.drawable.support),
+                            contentDescription = null
+                        )
+                    },
+                    label = { Text("Help") },
+                    selected = false,
+                    onClick = closeDrawer,
+                )
+                NavigationDrawerItem(
+                    shape = Shapes.NavigationItemShape,
+                    modifier = Modifier.padding(end = Dimens.gird_two),
+                    icon = {
+                        Icon(
+                            painterResource(id = R.drawable.cancel),
+                            contentDescription = null
+                        )
+                    },
+                    label = { Text("Exit") },
+                    selected = false,
+                    onClick = {
+                        (ctx as Activity).finishAndRemoveTask()
+                    },
+                )
+
+            }
         }
     }
 }
@@ -203,11 +212,11 @@ fun AuthUserNavigationItem(
     val onclick: () -> Unit
     if (userAuthenticated) {
         userAction = stringResource(id = R.string.sign_out)
-        icon = painterResource(id = R.drawable.logout_fill0_wght700_grad0_opsz24)
+        icon = painterResource(id = R.drawable.signout)
         onclick = signOut
     } else {
         userAction = stringResource(id = R.string.sign_in)
-        icon = painterResource(id = R.drawable.login_fill0_wght700_grad0_opsz24)
+        icon = painterResource(id = R.drawable.signin)
         onclick = signIn
     }
     NavigationDrawerItem(
@@ -228,13 +237,13 @@ fun AuthUserNavigationItem(
 @Preview
 @Composable
 fun DrawerContentPreview() {
-
     val ctx = LocalContext.current
     RobinAppPreview {
         DrawerContent(
             userAuthenticated = true,
             navController = NavHostController(ctx),
-            signOut = {}
-        ) {}
+            signOut = {},
+            closeDrawer = {}
+        )
     }
 }
