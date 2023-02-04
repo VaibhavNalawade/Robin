6acf6f62019bf0c8fba87d33c3a552d2558ceeae
vaibhav <vaibhavnalawade@outlook.in>
2023-02-04 19:18:45 +0530

    added path resources. removed unused files

--- app/src/main/java/com/vaibhav/robin/data/models/Brand.kt
+++
@@ -1,9 +0,0 @@
-package com.vaibhav.robin.data.models
-
-import com.google.firebase.firestore.DocumentReference
-
-data class Brand(
-    val name: String="",
-    val url: String="",
-    val ref: DocumentReference?=null
-)
--- app/src/main/java/com/vaibhav/robin/data/models/Price.kt
+++
@@ -1,7 +0,0 @@
-package com.vaibhav.robin.data.models
-
-data class Price(
-    val discountAvailable: Boolean = false,
-    val discountedPrice: Double = 0.0,
-    val retail: Double = 0.0,
-)
